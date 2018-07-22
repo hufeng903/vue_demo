d3996b7078420aa98d6a58e9f83958235f85272f
hufeng903 <hu-feng@hotmail.com>
2018-07-22 18:21:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!--模块-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="hello">
+     <h1>{{msg}}</h1>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '欢迎来到'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
